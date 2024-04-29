2faa32593c81fd305c286998f59d677f45fe2941
hellegg <heleg.chist@gmail.com>
2024-04-29 09:37:17 +0300

    database designed

--- db.sql
+++ db.sql
@@ -1,12 +1,46 @@
 -- 1. create tables
 CREATE TABLE IF NOT EXISTS nomenclature
 (
-    id    INTEGER PRIMARY KEY AUTOINCREMENT,
-    name  TEXT    NOT NULL,
-    price INTEGER NOT NULL
+    id      INTEGER PRIMARY KEY AUTOINCREMENT,
+    name    TEXT    NOT NULL,
+    price   INTEGER NOT NULL,
+    disable INTEGER DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS production
+(
+    id              INTEGER PRIMARY KEY AUTOINCREMENT,
+    created_at      TEXT DEFAULT CURRENT_TIMESTAMP       NOT NULL,
+    nomenclature_id INTEGER REFERENCES nomenclature (id) NOT NULL,
+    workday_id      INTEGER REFERENCES workday (id)      NOT NULL
+);
+
+CREATE INDEX IF NOT EXISTS idx_production_nomenclature ON production (nomenclature_id);
+
+CREATE INDEX IF NOT EXISTS idx_production_workday ON production (workday_id);
+
+CREATE TABLE IF NOT EXISTS workday
+(
+    id       INTEGER PRIMARY KEY AUTOINCREMENT,
+    start_at TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    end_to   TEXT
 );
 
 -- 2. insert test values
-INSERT INTO nomenclature (NAME, PRICE) VALUES ('Шпиндель', 123);
-INSERT INTO nomenclature (NAME, PRICE) VALUES ('Шток', 450);
-INSERT INTO nomenclature (NAME, PRICE) VALUES ('130/20/30', 1000);
+INSERT INTO nomenclature (id, name, price)
+VALUES (1, 'Шпиндель', 123),
+       (2, 'Шток', 450),
+       (3, '130/20/30', 1000);
+
+INSERT INTO workday (id, start_at, end_to)
+VALUES (1, '2024-04-12 12:14:46', '2024-04-12 21:04:33'),
+       (2, '2024-04-13 08:22:00', null);
+
+INSERT INTO production (id, created_at, nomenclature_id, workday_id)
+VALUES (1, '2024-04-12 13:00:00', 1, 1),
+       (2, '2024-04-12 14:00:00', 2, 1),
+       (3, '2024-04-12 18:00:00', 2, 1),
+       (4, '2024-04-12 21:00:00', 2, 1),
+       (5, '2024-04-13 08:00:00', 3, 2),
+       (6, '2024-04-13 09:00:00', 3, 2);
+
