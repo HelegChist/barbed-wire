ac8c9b09677ae647f0185f9a654078ebcbc5c74c
hellegg <heleg.chist@gmail.com>
2024-04-29 10:34:09 +0300

    today screen load word day

--- screens/TodayScreen.js
+++ screens/TodayScreen.js
@@ -1,10 +1,52 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { useSQLiteContext } from 'expo-sqlite/next';
+import { useIsFocused } from '@react-navigation/native';
 
 const TodayScreen = () => {
+    const db = useSQLiteContext();
+    const isFocused = useIsFocused();
+
+    const [newWorkday, setIsNewWorkday] = React.useState(true);
+    const [productions, setProductions] = React.useState([]);
+
+    React.useEffect(() => {
+        const id = getNotClosedDayId();
+        if (id) {
+            setProductions(loadWorkdayById(id));
+            setIsNewWorkday(false);
+        } else {
+            setIsNewWorkday(true);
+        }
+    }, [isFocused]);
+
+    const getNotClosedDayId = () => {
+        let firstSync = db.getFirstSync('SELECT * FROM workday w WHERE w.end_to IS NULL');
+        return firstSync.id;
+    }
+
+    const loadWorkdayById = (id) => {
+        return db.getAllSync(`
+            SELECT n.name, COUNT(n.name) as count, SUM(n.price) as sum
+            FROM workday w
+                JOIN production p
+            ON w.id = p.workday_id
+                JOIN nomenclature n ON n.id = p.nomenclature_id
+            WHERE w.id = ?
+            GROUP BY p.nomenclature_id;`, id);
+    }
+
+    if (newWorkday) {
+        return (
+            <View>
+                <Text>Создать новый день</Text>
+            </View>);
+    }
+
     return (
         <View>
-            <Text>Work Screen</Text>
+            <Text>${JSON.stringify(productions)}</Text>
+            <Text>Закрыть день</Text>
         </View>
     );
 };
