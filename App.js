598a86d1cd08eadb3d012b980998a16d7ded6cb9
hellegg <heleg.chist@gmail.com>
2024-05-07 22:22:37 +0300

    fix version 1.0.1

--- App.js
+++ App.js
@@ -1,31 +1,18 @@
 import React from 'react';
-import { ActivityIndicator, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import DarculaTheme from './constants/DarculaTheme';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import TabNavigators from './navigations/TabNavigators';
-import { deleteDatabase, loadDatabase } from './utils/FileUtils';
+import { loadDatabase } from './utils/FileUtils';
 import { SQLiteProvider } from 'expo-sqlite/next';
 
 function App() {
 
-    const [dbLoaded, setDbLoaded] = React.useState(false);
-
     React.useEffect(() => {
-        deleteDatabase().then(async () => {
-            loadDatabase()
-                .then(() => setDbLoaded(true))
-                .catch((e) => console.error(e));
-        })
+        loadDatabase()
+            .catch((e) => console.error(e));
     }, []);
 
-    if (!dbLoaded)
-        return (
-            <View style={{flex: 1}}>
-                <ActivityIndicator size={'large'}/>
-                <Text>Loading Database...</Text>
-            </View>
-        );
 
     return (
         <SafeAreaProvider>
